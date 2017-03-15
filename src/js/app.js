49a1807ffa59e167b1fcb8f002fcea2455348fae
adeliadjuarto <dj.adelia18@gmail.com>
2017-03-15 13:16:33 +0700

    service worker cache

--- src/js/app.js
+++ src/js/app.js
@@ -1,62 +1,26 @@
-(function () {
+// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+(function() {
   'use strict';
 
-  var cardElement = document.querySelector('.card');
-  var addCardBtnElement = document.querySelector('.add__btn');
-  var addCardInputElement = document.querySelector('.add__input');
-  var spinnerElement = document.querySelector('.card__spinner');
-  var bgSyncTextElement = document.querySelector('.bg-sync__text');
-  var bgSyncElement = document.querySelector('.custom__button-bg');
-
-  //Add github user data to the card
-  function addGitUserCard() {
-    var userInput = addCardInputElement.value;
-    if (userInput === '') return;
-    addCardInputElement.value = '';
-    localStorage.setItem('request', userInput);
-    fetchGitUserInfo(userInput);
+  // TODO add service worker code here
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./src/service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
   }
-
-  //Add card click event
-  addCardBtnElement.addEventListener('click', addGitUserCard, false);
-
-  //To get github user data via `Fetch API`
-  function fetchGitUserInfo(username, requestFromBGSync) {
-    var name = username || 'gokulkrishh';
-    var url = 'https://api.github.com/users/' + name;
-
-    spinnerElement.classList.add('show'); //show spinner
-
-    fetch(url, { method: 'GET' })
-    .then(function(fetchResponse){ return fetchResponse.json() })
-      .then(function(response) {
-        if (!requestFromBGSync) {
-          localStorage.removeItem('request'); //Once API is success, remove request data from localStorage
-        }
-        cardElement.querySelector('.card__title').textContent = response.name;
-        cardElement.querySelector('.card__desc').textContent = response.bio;
-        cardElement.querySelector('.card__img').setAttribute('src', response.avatar_url);
-        cardElement.querySelector('.card__following span').textContent = response.following;
-        cardElement.querySelector('.card__followers span').textContent = response.followers;
-        cardElement.querySelector('.card__temp span').textContent = response.company;
-        spinnerElement.classList.remove('show'); //hide spinner
-      })
-      .catch(function (error) {
-        //If user is offline and sent a request, store it in localStorage
-        //Once user comes online, trigger bg sync fetch from application tab to make the failed request
-        localStorage.setItem('request', name);
-        spinnerElement.classList.remove('show'); //hide spinner
-        console.error(error);
-      });
-  }
-
-  fetchGitUserInfo(localStorage.getItem('request')); //Fetch github users data
-
-  //Listen postMessage when `background sync` is triggered
-  navigator.serviceWorker.addEventListener('message', function (event) {
-    console.info('From background sync: ', event.data);
-    fetchGitUserInfo(localStorage.getItem('request'), true);
-    bgSyncElement.classList.remove('hide'); //Once sync event fires, show register toggle button
-    bgSyncTextElement.setAttribute('hidden', true); //Once sync event fires, remove registered label
-  });
+  
 })();

47e592494d3ff049758b73683bd0e9dfa32f61fa
adeliadjuarto <dj.adelia18@gmail.com>
2017-03-17 09:10:36 +0700

    tambah animation slide sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style, transition, animate } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        left: '-150px'
+      })),
+      state('out', style({
+        left: '0px'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class AppComponent {
   title = 'app works!';
 
-  showElement:boolean = false;
-
-    toggle(){
-    this.showElement = !this.showElement;
-  }
+  menuState:string = 'out';
+    toggleMenu(){
+      this.menuState = this.menuState === 'out' ? 'in' : 'out';
+    }
 }

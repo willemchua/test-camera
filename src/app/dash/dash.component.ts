3e203418c6c30dee09d89737d3ace87b2b118a52
adeliadjuarto <dj.adelia18@gmail.com>
2017-04-05 15:18:16 +0700

    add line chart primeNG

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dash.component.css']
 })
 export class DashComponent implements OnInit {
-  data:any;
+  doughnutData:any;
+  lineData:any;
+
   constructor() { 
-    this.data = {
+    this.doughnutData = {
       labels: ['A','B','C'],
       responsive:true,
       animation: true,
@@ -27,6 +29,23 @@ export class DashComponent implements OnInit {
               ]
           }]    
       };
+      this.lineData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'First Dataset',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                borderColor: '#4bc0c0'
+            },
+            {
+                label: 'Second Dataset',
+                data: [28, 48, 40, 19, 86, 27, 90],
+                fill: false,
+                borderColor: '#565656'
+            }
+        ]
+    }
   }
 
   ngOnInit() {

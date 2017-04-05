5d8974ea764ff7c08c33378c029cdc0c346eee70
adeliadjuarto <dj.adelia18@gmail.com>
2017-04-05 15:30:12 +0700

    add chart

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -6,8 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dash.component.css']
 })
 export class DashComponent implements OnInit {
-
-  constructor() { }
+  doughnutData:any;
+  lineData:any;
+  
+  constructor() { 
+      this.doughnutData = {
+      labels: ['A','B','C'],
+      responsive:true,
+      animation: true,
+      datasets: [
+          {
+              data: [300, 50, 100],
+              backgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56"
+              ],
+              hoverBackgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56"
+              ]
+          }]    
+      };
+      this.lineData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'First Dataset',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                borderColor: '#4bc0c0'
+            },
+            {
+                label: 'Second Dataset',
+                data: [28, 48, 40, 19, 86, 27, 90],
+                fill: false,
+                borderColor: '#565656'
+            }
+        ]
+    }
+  }
 
   ngOnInit() {
   }

d1d7bc84975702699d5c79982748343bba0f01b3
adeliadjuarto <dj.adelia18@gmail.com>
2017-04-06 16:09:05 +0700

    create circles

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -9,10 +9,12 @@ import { D3Service, D3, Selection } from 'd3-ng2-service';
 export class DashComponent implements OnInit {
   private d3: D3;
   private parentNativeElement: any;
+  private data: any;
 
   constructor(element: ElementRef, d3Service: D3Service) { 
       this.d3 = d3Service.getD3();
       this.parentNativeElement = element.nativeElement;
+      this.data = [150, 50];
   }
 
   ngOnInit() {
@@ -20,8 +22,10 @@ export class DashComponent implements OnInit {
     let d3ParentElement: Selection<any, any, any, any>;
 
     if (this.parentNativeElement !== null) {
-        let container = d3.select('.chart').append('svg').attr('width',200).attr('height',200);
-        container.append('circle').attr('cx',50).attr('cy',50).attr('r', 30);
+        let container = d3.select('.chart').append('svg').attr('width',400).attr('height',400);
+        let circles = container.selectAll('circle').data(this.data).enter().append('circle');
+
+        circles.attr('cx', (d:any) => { return d }).attr('cy', (d:any) => { return d }).attr('r', 50);
     }
   }
 
